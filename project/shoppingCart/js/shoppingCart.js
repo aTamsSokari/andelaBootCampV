acb78f084a3a8274d6958989f7c7e953e3cf15fa
aTamsSokari <atamssokari@yahoo.com>
2015-02-18 14:29:13 +0100

    latest changes

--- project/shoppingCart/js/shoppingCart.js
+++ project/shoppingCart/js/shoppingCart.js
@@ -1,3 +1,80 @@
-// JavaScript source code
-//storeItem drag and drop
-//draggable is true
+var store = document.querySelector('#storeItems');
+var cart = document.querySelector('#cartItems');
+
+function addToCart(name) {
+  this.name = document.getElementsByClassName('itemName');
+  var addItem = document.getElementsByClassName('.cartItem');
+  var newCartItem = document.createElement('ul');
+  var cartItemContent = document.innerHTML()
+
+}
+
+//event functions from html5rocks.com's tutorial
+
+//to start drag
+function handleDragStart(e) {
+  this.style.opacity = '0.4';  // this / e.target is the source node.
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+//to carry out enter, hover, leave
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault(); // Necessary. Allows us to drop.
+  }
+
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  // this / e.target is the current hover target.
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');  // this / e.target is previous target element.
+}
+
+//handle drop and drag End
+function handleDrop(e) {
+  // this / e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // stops the browser from redirecting.
+  }
+  // See the section on the DataTransfer object.
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+
+  [].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });
+}
+
+
+var item = document.querySelectorAll('.item');
+[].forEach.call(item, function (item) {
+  item.addEventListener('dragstart', handleDragStart, false);
+  item.addEventListener('dragenter', handleDragEnter, false);
+  item.addEventListener('dragover', handleDragOver, false);
+  item.addEventListener('dragleave', handleDragLeave, false);
+  col.addEventListener('drop', handleDrop, false);
+  col.addEventListener('dragend', handleDragEnd, false);
+});
