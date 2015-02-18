b2414f986586c461c1aaefb1d32e0378a4df54bb
aTamsSokari <atamssokari@yahoo.com>
2015-02-18 17:05:05 +0100

    update

--- project/shoppingCart/js/shoppingCart.js
+++ project/shoppingCart/js/shoppingCart.js
@@ -1,13 +1,70 @@
-var store = document.querySelector('#storeItems');
-var cart = document.querySelector('#cartItems');
+$('.item')
+  .bind('dragstart', function (evt) {
+    evt.dataTransfer.setData('text', this.id);
+    $('h2').fadeIn('fast');
+  })
+  .hover(function () { $('div', this).fadeIn(); },
+  function () { $('div', this).fadeOut(); }
+);
 
-function addToCart(name) {
-  this.name = document.getElementsByClassName('itemName');
-  var addItem = document.getElementsByClassName('.cartItem');
-  var newCartItem = document.createElement('ul');
-  var cartItemContent = document.innerHTML()
+$('#cartItems')
+	.bind('dragover', function (evt) {
+	  evt.preventDefault();
+	})
+	.bind('dragenter', function (evt) {
+	  evt.preventDefault();
+	})
+  .bind('drop', function (evt) {
+    var id = evt.dataTransfer.getData('text'),
+      item = $('#' + id),
+			cartList = $("#cartItems ul"),
+			prevCartItem = null,
+			notInCart = (function () {
+			  var lis = $('li', cartList),
+					len = lis.length,
+					i;
 
-}
+			  for (i = 0; i < len; i++) {
+			    var temp = $(lis[i]);
+			    if (temp.data('id') === id) {
+			      prevCartItem = temp;
+			      return false;
+			    }
+			  }
+			  return true;
+			}()),
+			quantLeftEl, quantBoughtEl, quantLeft;
+
+    $("h2").fadeOut('fast');
+
+  if (notInCart) {
+    prevCartItem = $('<li />', {
+      text : $('p:first', item).text(),
+      data : { id : id }
+    }).prepend($('<span />', {
+      'class' : 'quantity',
+      text : '0'
+    })).prepend($('<span />', {
+      'class' : 'price',
+      text : price
+    })).appendTo(cartList);
+  }
+
+  quantLeftEl = $('p:last span', item);
+  quantLeft   = parseInt(quantLeftEl.text(), 10) - 1;
+  quantBoughtEl = $('.quantity', prevCartItem);
+  quantBoughtEl.text(parseInt(quantBoughtEl.text(), 10) + 1);
+  quantLeftEl.text(quantLeft);
+
+  if (quantLeft === 0) {
+    item.fadeOut('fast');
+  }
+
+  total.text((parseFloat(total.text(), 10) + parseFloat(price.split('$')[1])).toFixed(2));
+
+  evt.stopPropagation();
+  return false;
+  });
 
 //event functions from html5rocks.com's tutorial
 
@@ -78,3 +135,6 @@ var item = document.querySelectorAll('.item');
   col.addEventListener('drop', handleDrop, false);
   col.addEventListener('dragend', handleDragEnd, false);
 });
+
+
+
