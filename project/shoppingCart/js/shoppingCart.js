52cfa0d9565c01b067118127dbe9fe52f523d2df
aTamsSokari <atamssokari@yahoo.com>
2015-02-19 14:07:47 +0100

    latest changes, i'm almost there

--- project/shoppingCart/js/shoppingCart.js
+++ project/shoppingCart/js/shoppingCart.js
@@ -1,91 +1,70 @@
-var dragSrcEl = null;
+$('.item')
+  .bind('dragstart', function (e) {
+    this.style.opacity = '0.6';  // this / e.target is the source node.
+    e.dataTransfer.setData('text', this.innerHTML);
+    $('h2').fadeIn('fast');
+  })
+  .hover(
+    function () { $('div', this).fadeIn(); }, 
+    function () { $('div', this).fadeOut(); }
+    );
 
-function dragStart(e) {
-  this.style.opacity = '0.6';  // this / e.target is the source node.
+$('#cartItems')
+  .bind('dragover', function (e) {
+    e.preventDefault();
+  })
+  .bind('dragenter', function (e) {
+    e.preventDefault();
+  })
+  .bind('drop', function (e) {
+    var id = e.dataTransfer.getData('text'),
+        item = $('#' + id),
+        cartList = $("#cartItems ul"),
+        total = $("#total span"),
+        prevCartItem = null,
+        emptyCart = (function () {
+          var list = $('li', cartList),
+            l = list.length,
+            i;
 
-  var dragSrcEl = this;
-  $('h2').fadeIn('fast');
-
-  e.dataTransfer.effectAllowed = 'copymove';
-  e.dataTransfer.setData('text', this.id);
-}
-
-function dragOver(e) {
-  evt.preventDefault();
-  e.dataTransfer.dropEffect = 'copy';  // See the section on the DataTransfer object.
-  return false;
-}
-
-function dragEnter(e) {
-  if (event.target.id == cartItems) {
-    event.target.style.background = 'brown';
-  }
-  evt.preventDefault();
-}
-
-function drop(e) {
-  var id = e.dataTransfer.getData('text'),
-      item = $('#' + id),
-      cartList = $('#cartItems ul'),
-      totalItems = $('#total span'),
-      prevCartItem = null,
-      emptyCart = (function () {
-        var list = $('li', cartList),
-					l = list.length,
-					i;
-
-        for (i = 0; i < l; i++) {
-          var temp = $(l[i]);
-          if (temp.data('id') === id) {
-            prevCartItem = temp;
-            return false;
+          for (i = 0; i < l; i++) {
+            var temp = $(l[i]);
+            if (temp.data('id') === id) {
+              prevCartItem = temp;
+              return false;
+            }
           }
-        }
-        return true;
-      }),
-      quanInStore,
-      quanInCart,
-      quanLeft;
+          return true;
+        }()),
+        quanInStore, quanInCart, quanLeft;
 
-  $('h2').fadeOut('fast');
+    $("h2").fadeOut('fast');
 
-  if (emptyCart) {
-    prevCartItem = $('<li />', {
-      text: $('p:first', item).text(),
-      data: { id: id }
-    }).prepend($('<span />', {
-      'class': 'quantity',
-      text: '0'
-    })).appendTo(cartList);
-  }
+    if (emptyCart) {
+      prevCartItem = $('<li />', {
+        text: $('p:first', item).text(),
+        data: { id: id }
+      }).prepend($('<span />', {
+        'class': 'quantity',
+        text: '0'
+      })).prepend($('<span />', {
+        'class': 'price',
+        text: price
+      })).appendTo(cartList);
+    }
 
-  quanInStore = $('p:last span', item);
-  quanLeft = parseInt(quanInStore.text(), 10) - 1;
-  quanInCart = $('.quantity', prevItem);
-  quanInCart.text(parseInt(quanInCart.text(), 10) + 1);
-  quanInStore.text(quanLeft);
+    quanInStore = $('p:last span', item);
+    quanLeft = parseInt(quanInStore.text(), 10) - 1;
+    quanInCart = $('.quantity', prevCartItem);
+    quanInCart.text(parseInt(quanInCart.text(), 10) + 1);
+    quanInStore.text(quanLeft);
 
-  if (quanLeft === 0) {
-    item.fadeOut('fast');
-  }
+    if (quanLeft === 0) {
+      item.fadeOut('fast');
+    }
 
-  total.text((parseFloat(total.text(), 10)));
+    total.text((parseFloat(total.text(), 10)));
 
-  evt.stopPropagation();
-  return false;
-
-};
-var items = document.querySelectorAll('.item');
-[].forEach.call(items, function (items) {
-  items.addEventListener('dragstart', dragStart, false);
-});
-$(items).hover(function () { $('div', this).fadeIn(); },
-  function () {
-    $('div', this).fadeOut();
+    e.stopPropagation();
+    return false;
   });
-
-
-var cart = document.querySelector('#cartItems');
-cart.addEventListener('dragenter', dragEnter, false);
-cart.addEventListener('dragover', dragOver, false);
-cart.addEventListener('drop', drop, false);
